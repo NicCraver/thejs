71a80fa2efd48b7da44d76ccb3c69dd3d0d108fe
NicCraver <951477037@qq.com>
2020-07-27 14:53:19 +0800

    update

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-// 获取当前时间年月日
-module.exports.getNowDate = () => {
-  // 格式为 2020-10-10
-  let date = new Date();
-  return date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
-};
+// // 获取当前时间年月日
+// module.exports.getNowDate = () => {
+//   // 格式为 2020-10-10
+//   let date = new Date();
+//   return date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
+// };
+
+
+const nicjs = require('nicjs')
+
+console.log('nicjs.getNowDate()', nicjs.getNowDate())
